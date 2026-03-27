19d4f74cc04dc57776031961aabfef35c35c2138
ARUUKE <muratbekovaaruuke01@kstu.kg>
2026-03-28 00:03:24 +0600

    privetstvie drugih poni

--- pony.go
+++ pony.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Flattershy!")
+	fmt.Println("Hello, Fluttershy!")
+	fmt.Println("Hello, Rarity!")
+	fmt.Println("Hello, Applejack!")
 }
