e2ab686fe4a18aef692783c61668092f44620ba3
ARUUKE <muratbekovaaruuke01@kstu.kg>
2026-03-28 00:24:27 +0600

    изменение мэйн.го

--- pony.go
+++ pony.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
-func PonyMessage() {
-	fmt.Println("This is Pony file")
+func main() {
+	fmt.Println("Привет, мир! Я люблю тебя ❤")
+
+	age := 17
+	fmt.Printf("Через 5 лет мне будет %d лет\n", age+5)
+
+	names := []string{"Анеля", "Нурболот", "Аэлита"}
+	fmt.Println("Список друзей:")
+	for _, name := range names {
+		fmt.Println("-", name)
+	}
 }
